51924d2b83f83eb40fd04c6a65fcbdd48e5d65ef
Thiago Gonzalez <contato@thiagogonzalez.com>
2017-02-04 17:12:48 -0200

    Finish the TODO list

---
+++ app/assets/javascripts/todo-list.js
@@ -0,0 +1,47 @@
+var itemInput = $('#input-item'),
+    addButton = $('#button-item'),
+    itemsHolder = $('#list-items');
+
+var createNewItemElement = function(itemString) {
+  var listItem = document.createElement("li");
+  var text = document.createElement("span");
+  var deleteButton = document.createElement("button");
+
+  $(deleteButton).text("Delete");
+  $(deleteButton).addClass("delete");
+
+  $(text).text(itemString);
+
+  $(listItem).append(text);
+  $(listItem).append(deleteButton);
+
+  return listItem;
+}
+
+var addItem = function() {
+  var listItem = createNewItemElement(itemInput.val());
+  itemsHolder.append(listItem);
+  bindItemEvents(listItem);
+
+  itemInput.val('');
+}
+
+var bindItemEvents = function(listItem) {
+  var deleteButton = $(listItem).find("button.delete");
+
+  deleteButton.on('click', function() {
+    itemsHolder.find($(this).parent()).remove();
+  });
+}
+
+
+// Call addItem
+$(addButton).on('click', function() {
+  addItem();
+});
+
+$(itemInput).keyup(function(e){
+  if(e.keyCode == 13) {
+    addItem();
+  }
+});
